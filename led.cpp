ffae5d07b8240b1d396eb8bc860a46f084ae649f
Alexis Brard <alexis.brard@insa-rennes.fr>
2017-07-31 15:34:27 +0200

    Created tables to store Leds and its RGB components

--- led.cpp
+++ led.cpp
@@ -1,6 +1,6 @@
 #include "led.h"
 
-//
+// Turn on a Led with the RGB components in parameter
 void lightLed(Led led, int r, int g, int b) {
   Tlc.set(led.red, r);                           // Red component of the LED
   Tlc.set(led.green, g);                         // Green component of the LED
--- led_tower.ino
+++ led_tower.ino
@@ -2,19 +2,36 @@
 #include "Tlc5940.h"
 #include "led.h"
 
-Led led1;
-Led led2;
+
+// Initialization of a table of Leds and its RGB components
+Led leds[16];
+int red[16];
+int green[16];
+int blue[16];
 
 void setup() {
-  led1.red = 0;
-  led1.green = 16;
-  led1.blue = 32;
   
-  led2.red = 1;
-  led2.green = 17;
-  led2.blue = 33;
+  // Association of Leds to its RGB components
+  for (int i=0 ; i<16 ; ++i){
+    leds[i].red = i;
+    leds[i].green = i + 16;
+    leds[i].blue = i + 32;
+  }
+
+  // TLC initialization
+  Tlc.init();
+  Tlc.clear();
 }
 
 void loop() {
-
+  lightLed(leds[0], 255, 0, 0);
+  lightLed(leds[1], 0, 0, 255);
+  lightLed(leds[3], 0, 255, 0);
+  Tlc.update();
+  delay(1000);
+  
+  lightLed(leds[0], 0, 0, 255);
+  lightLed(leds[1], 255, 0, 0);
+  Tlc.update();
+  delay(1000);
 }
