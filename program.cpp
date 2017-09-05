a66fd14492fb52d7e7ae3c6672fb2c8265459389
Alexis Brard <alexis.brard@insa-rennes.fr>
2017-09-05 16:12:36 +0200

    Added an independant program : led scrolling with a color gradient

--- program.cpp
+++ program.cpp
@@ -22,3 +22,26 @@ void independantProgram1(int color, int updates, int * counter, Led * leds, int 
    *counter = 0;
   
 }
+
+
+// Led scrolling, one color defined by the user with the color potentiometer
+void independantProgram2(int color, int updates, int * counter, Led * leds, int * red, int * green, int * blue, int * predefined_red, int * predefined_green, int * predefined_blue){
+  
+  predefinedColors(color, predefined_red, predefined_green, predefined_blue);
+  
+  updates = 10 + updates / 50;
+
+  while(!graduallyTurnOn(leds[*counter], predefined_red[*counter], predefined_green[*counter], predefined_blue[*counter], &red[*counter], &green[*counter], &blue[*counter])) {
+    if (*counter > 0)
+      graduallyTurnOff(leds[(*counter)-1], &red[(*counter)-1], &green[(*counter)-1], &blue[(*counter)-1]);
+    else
+      graduallyTurnOff(leds[15], &red[15], &green[15], &blue[15]);
+    delay(updates);
+    Tlc.update();
+  }
+ 
+ (*counter)++;
+ if (*counter > 15)
+   *counter = 0;
+  
+}
--- program.h
+++ program.h
@@ -7,6 +7,7 @@
 
 
 void independantProgram1(int color, int updates, int * counter, Led * leds, int * red, int * green, int * blue, int * predefined_red, int * predefined_green, int * predefined_blue);
+void independantProgram2(int color, int updates, int * counter, Led * leds, int * red, int * green, int * blue, int * predefined_red, int * predefined_green, int * predefined_blue);
 
 
 #endif // PROGRAM_H
